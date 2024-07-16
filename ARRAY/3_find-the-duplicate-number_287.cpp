cdee464123b086c204aa84e3c1c534bfec720e5b
h4-harshh <harshnew2204@gmail.com>
2024-07-16 10:58:33 +0530

    day 1

---
+++ ARRAY/3_find-the-duplicate-number_287.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+
+        // initialize the two pointer
+        int slow = nums[0];
+        int fast = nums[0];
+
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = nums[0];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return fast;
+    }
+};
