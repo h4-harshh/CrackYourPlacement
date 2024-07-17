82f89a23e88a21efcc7e29b31e975f177540a6c5
h4-harshh <harshnew2204@gmail.com>
2024-07-17 14:02:30 +0530

    day 2

---
+++ ARRAY/12_container-with-most-water_11.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+
+        int low = 0;
+        int high = height.size() - 1;
+
+        int res = 0;
+
+        while (low < high)
+        {
+            int curr_area = min(height[low], height[high]) * (high - low);
+            res = max(res, curr_area);
+
+            if (height[low] < height[high])
+            {
+                low++;
+            }
+            else
+            {
+                high--;
+            }
+        }
+        return res;
+    }
+};
