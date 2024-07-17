0958fcd03cd70ec648b417b8f37369ce5820fce8
HARSH <mailtoharsh22@gmail.com>
2024-07-17 14:28:24 +0530

    Time: 61 ms (79.64%) | Memory: 45.4 MB (49.08%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,22 +4,26 @@ public:
         int n=nums.size();
         int count=0;
 
-        vector<int>prefix(n,0);
-        prefix[0]=nums[0];
+        // vector<int>prefix(n,0);
+        // prefix[0]=nums[0];
 
-        for(int i=1;i<n;i++)
-        {
-            prefix[i]=nums[i]+prefix[i-1];
-        }
+        // for(int i=1;i<n;i++)
+        // {
+        //     prefix[i]=nums[i]+prefix[i-1];
+        // }
+
+        int sum=0;
 
         unordered_map<int,int>mp;
         mp[0]=1;
         for(int i=0;i<n;i++)
         {
-            if(mp.find(prefix[i]-k)!=mp.end()){
-                count+=mp[prefix[i]-k];
+            sum+=nums[i];
+            int req=sum-k;
+            if(mp.find(req)!=mp.end()){
+                count+=mp[req];
             }
-                mp[prefix[i]]++;
+                mp[sum]++;
         }
         return count;
     }
