ea3ffd8f31926226569268d04e75074c1c196e8b
HARSH <mailtoharsh22@gmail.com>
2024-07-19 16:30:42 +0530

    Time: 21 ms (25.39%) | Memory: 19.2 MB (10.91%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>>result;
+
+    void solve(unordered_map<int,int>mp,vector<int>temp)
+    {
+
+        if(temp.size()==n)
+        {
+            result.push_back(temp);
+            return;
+        }
+        for(auto &[num,count]:mp)
+        {
+            if(count==0) continue;
+
+            temp.push_back(num);
+            mp[num]--;
+
+            solve(mp,temp);
+
+            temp.pop_back();
+            mp[num]++;
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n=nums.size();
+
+        unordered_map<int,int>mp;
+        for(auto &num:nums)
+        {
+            mp[num]++;
+        }
+
+        vector<int>temp;
+        solve(mp,temp);
+
+        return result;
+    }
+};
