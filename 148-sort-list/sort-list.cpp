01b61c86bfd43c3c6e088b0e20112f50bbc2fc00
HARSH <mailtoharsh22@gmail.com>
2024-08-03 06:28:38 +0530

    Time: 158 ms (15.54%) | Memory: 74.8 MB (22.53%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -8,30 +8,57 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- //approach 1->using array
 class Solution {
 public:
+    ListNode *find_middle(ListNode *head)
+    {
+        ListNode *slow=head;
+        ListNode *fast=head->next;
+        while(fast!=nullptr && fast->next!=nullptr)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode *merge(ListNode *left,ListNode *right)
+    {
+        ListNode *dummy=new ListNode(0);
+        ListNode *curr=dummy;
+
+        while(left && right)
+        {
+            if(left->val<=right->val)
+            {
+                curr->next=left;
+                curr=curr->next;
+                left=left->next;
+            }
+            else
+            {
+                curr->next=right;
+                curr=curr->next;
+                right=right->next;
+            }
+        }
+        if(left) curr->next=left;
+        else curr->next=right;
+
+        return dummy->next;
+    }
     ListNode* sortList(ListNode* head) {
-        if(head==nullptr) return head;
-        ListNode *curr=head;
 
-        vector<int>arr;
-        while(curr)
-        {
-            arr.push_back(curr->val);
-            curr=curr->next;
-        }
-        sort(arr.begin(),arr.end());
+        if(head==nullptr || head->next==nullptr) return head;
 
-        int i=0;
-        curr=head;
-        while(curr)
-        {
-            curr->val=arr[i];
-            curr=curr->next;
-            i++;
-        }
-        return head;
+        ListNode *middle=find_middle(head);
+
+        ListNode *right=middle->next;
+        middle->next=nullptr;
+        ListNode *left=head;
+
+        left=sortList(left);
+        right=sortList(right);
+
+        return merge(left,right);      
     }
 };
